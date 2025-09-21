ccbcb199f8c0ae1448a059876f7188bae09a72d3
Aman Khan <amanjkhan21@gmail.com>
2025-09-21 19:02:45 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 document.getElementById("login-btn").addEventListener("click", function () {
     window.location.href = "/html/login.html";
 });
+
+document.getElementById("sign-up-btn").addEventListener("click", function () {
+    window.location.href = "/html/signup.html";
+});
+
+document.getElementById("join-now-btn").addEventListener("click", function () {
+    window.location.href = "/html/signup.html";
+});
