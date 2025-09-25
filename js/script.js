ddd15b5668c6a8d8dcb9c1e056db627e89c16b95
Aman Khan <amanjkhan21@gmail.com>
2025-09-25 11:04:38 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
-document.getElementById("login-btn").addEventListener("click", function () {
-    window.location.href = "/html/login.html";
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("login-btn-index");
+    if (btn) {
+        btn.addEventListener("click", () => {
+            window.location.href = "html/login.html";
+        });
+    }
 });
 
-document.getElementById("sign-up-btn").addEventListener("click", function () {
-    window.location.href = "/html/signup.html";
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("signup-btn-index");
+    if (btn) {
+        btn.addEventListener("click", () => {
+            window.location.href = "html/signup.html";
+        });
+    }
 });
 
-document.getElementById("join-now-btn").addEventListener("click", function () {
-    window.location.href = "/html/signup.html";
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("join-now-btn-index");
+    if (btn) {
+        btn.addEventListener("click", () => {
+            window.location.href = "html/signup.html";
+        });
+    }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("login-btn-login");
+    if (btn) {
+        btn.addEventListener("click", () => {
+            window.location.href = "admin.html";
+        });
+    }
 });
